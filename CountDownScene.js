fdc2ef3e9f8906dc52e478507d780072737da0f6
GloriaTian <vgloria@g.ucla.edu>
2016-11-28 04:57:41 -0800

    merge version without signin/out

--- CountDownScene.js
+++ CountDownScene.js
@@ -28,19 +28,12 @@ import {
   ListItem
 } from 'react-native-elements';
 
+import Icon from 'react-native-vector-icons/FontAwesome';
 import EventScene from './Event';
 import LinearGradient from 'react-native-linear-gradient';
-import Icon from 'react-native-vector-icons/FontAwesome';
+
 
 export default class CountDownScene extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      selectedTab: 'firstTab',
-      date: new Date(),
-      loaded: false
-    };
-  }
   static get defaultProps() {
     return {
       title: 'Count Down'
@@ -52,19 +45,27 @@ export default class CountDownScene extends Component {
     password: PropTypes.string.isRequired,
   }
 
-  _goEventDetail() {
+  state = {
+    date: new Date(),
+    loaded: false,
+    length: 0
+  }
+
+  _goEventDetail(id) {
     this.props.navigator.push({
       component: EventScene,
-      title: 'EventScene'
+      title: 'EventScene',
+      passProps: { event_id: id }
     });
   }
 
   _dateFormat() {
     var monthNames = ["Janunary", "Februrary", "March", "April", "May", "June", "July",
       "August", "September", "October", "November", "December"];
-    var dayNames = ["Monday", "Tuesday", "Wednesday", "Thurday", "Friday", "Saturday", "Sunday"];
-    return dayNames[this.state.date.getDay()-1] + ',\n' +
-           this.state.date.getDate() + "th " + monthNames[this.state.date.getMonth()-1];
+    var dayNames = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
+    return dayNames[this.state.date.getDay()] + ',\n' +
+           this.state.date.getDate() + "th " + monthNames[this.state.date.getMonth()];
+    // return "day: " + new Date().getDay() + "date: " + new Date().getDate() + "month: " + this.state.date.getMonth();
   }
 
   _fetchData() {
@@ -72,14 +73,23 @@ export default class CountDownScene extends Component {
     return fetch(URL)
       .then((response) => response.json())
       .then((responseJson) => {
-        var state = {};
-          state['size'] = responseJson.res.timeStamp_list.length;
-          for (var i = 0; i < responseJson.res.timeStamp_list.length; i++) {
+        var state = [];
+        var len = responseJson.res.timeStamp_list.length;
+          state['size'] = len*3;
+          for (var i = 0; i < len; i++) {
             state[i] = responseJson.res.timeStamp_list[i].deadline;
           }
+          for (var i = len; i < 2*len; i++) {
+            state[i] = responseJson.res.timeStamp_list[i-len].description;
+          }
+          for (var i = 2*len; i < 3*len; i++) {
+            state[i] = String(responseJson.res.timeStamp_list[i-2*len].id);
+          }
+
         this.setState(state);
         this.setState({
           loaded : true,
+          length : len
         });
         // return events;
       })
@@ -90,40 +100,70 @@ export default class CountDownScene extends Component {
       .done();
   }
 
-	render() {
+  _parseDate(dateString) {
+    var monthtoDay = {
+      "Jan" : 0,
+      "Feb" : 1,
+      "Mar" : 2,
+      "Apr" : 3,
+      "May" : 4,
+      "Jun" : 5,
+      "Jul" : 6,
+      "Aug" : 7,
+      "Sep" : 8,
+      "Oct" : 9,
+      "Nov" : 10,
+      "Dec" : 11
+    };
+
+    var eventDate = parseInt( dateString.substring(5,7) );
+    var eventMonth = monthtoDay[ dateString.substring(8,11) ];
+    var eventYear = parseInt( dateString.substring(12,17) );
+    return new Date(eventYear, eventMonth, eventDate);
+  }
+
+  render() {
     var URL = 'https://hitch.herokuapp.com/api/getTimeStamp?event_id=2';
 
     var events = [];
-
+    
 
     if (this.state.loaded == false) {
       this._fetchData();
     }
 
 
-    var oneDay = 24*60*60*1000; // hours*minutes*seconds*milliseconds
+    for (var i = 0; i < this.state.length; i++) {
+      var eventDateString = this.state[i];
+      var eventDateObj = this._parseDate(eventDateString);
+      
+      var oneDay = 24*60*60*1000; // hours*minutes*seconds*milliseconds
+      var diffDays = Math.ceil(Math.abs(eventDateObj.getTime() - this.state.date.getTime())/oneDay);
+      var id = parseInt(this.state[this.state.length*2 + i]);
 
-    for (var i = 0; i < this.state.size; i++) {
-      //var diffDays = Math.round(Math.abs((this.state.time - this.state.time)/(oneDay)));
+      // AlertIOS.alert("id", id+ '....');
+
       events.push(<ListItem key={i}
-                      title={'Days left'}
+                      title={id + ' Days left'}
                       titleStyle={{fontSize: 22, color: '#eeeae5'}}
                       subtitle={this.state[i]}
                       subtitleStyle={{fontSize: 15, color: 'white'}}
+                      leftIcon={{name: 'clock-o', type: 'font-awesome', color: '#eeeae5'}}
                       containerStyle={{backgroundColor: 'transparent'}}
-                      onPress={() => this._goEventDetail()}>
+                      onPress={() => this._goEventDetail(this.props.event_id)}
+                      event_id={id} >
 
                   </ListItem>
                   );
     }
 
 
-		return (
+    return (
       <View style={{flex:1, flexDirection: 'column'}}>
         <View style={{height:620}}>
         <LinearGradient colors={['#1F2F3C', '#3D5167', '#5C7894', '#7C9AAF', '#97B2BE']}
                         style={styles.linearGradient}>
-          <View style={{height: 100, justifyContent: 'center',alignItems:'center'}}>
+          <View style={{height: 60, justifyContent: 'center',alignItems:'center'}}>
           </View>
           <View>
             <Text style={{color: '#eeeae5', fontSize: 40, textAlign: 'left', backgroundColor: 'rgba(0,0,0,0)',
@@ -147,8 +187,8 @@ export default class CountDownScene extends Component {
           barTintColor="gainsboro"
           backgroundColor = "azure">
           <Icon.TabBarItemIOS
-            iconName="list"
-            title="MyJobs"
+            iconName="clock-o"
+            title="CountDown"
             selected={this.state.selectedTab === 'firstTab'}
             iconColor={"grey"}
             selectedIconColor={'#1F2F3C'}
@@ -194,7 +234,7 @@ export default class CountDownScene extends Component {
             selectedIconColor={'#1F2F3C'}
             renderAsOriginal={true}
             onPress={() => {
-              this.props.navigator.push({
+              this.props.navigator.replace({
                   component: JobList,
                   title: 'Job List',
                   passProps: {
@@ -227,19 +267,18 @@ export default class CountDownScene extends Component {
           </Icon.TabBarItemIOS>
         </TabBarIOS>
         </View>
-
       </View>
-		)
-	};
+    )
+  };
 }
 
 var styles = StyleSheet.create({
   container: {
     backgroundColor: 'rgba(0,0,0,0)',
-    height: 0,
+    height: 620,
   },
   linearGradient: {
-    height: 800,
+    height: 620,
     width: Dimensions.get('window').width
   }
 });
---
+++ JobDetail.js
@@ -0,0 +1,178 @@
+import React, { Component, PropTypes } from 'react';
+import {
+  AppRegistry,
+  StyleSheet,
+  TouchableHighlight,
+  Text,
+  Image,
+  TextInput,
+  View,
+  NavigatorIOS,
+  ScrollView,
+  AlertIOS,
+} from 'react-native';
+import {
+  Button, List, ListItem, CheckBox, SearchBar, Icon, Tabs, Tab,
+} from 'react-native-elements'
+
+
+import NavigationBar from 'react-native-navbar';
+
+import Comment from './Comment'
+import AddJobForm from './AddJobForm';
+import AddJobFormAuto from './AddJobFormAuto';
+import Google from './Google';
+import DynamicList from './DynamicList'
+
+var REQUEST_URL = 'http://api.glassdoor.com/api/api.htm?t.p=108386&t.k=gOGr6axYbOq&userip=172.91.91.28&useragent=Mozilla/5.0%2520(Macintosh;%2520Intel%2520Mac%2520OS%2520X%252010_11_6)%2520AppleWebKit/537.36%2520(KHTML,%2520like%2520Gecko)%2520Chrome/54.0.2840.98%2520Safari/537.36&format=json&v=1&action=employers&q=software%2527'
+var Swipeout = require('react-native-swipeout')
+
+var styles = StyleSheet.create({
+  background:
+  {
+    flex:1, 
+  },
+  container: {
+    padding: 5,
+    marginBottom: 10,
+    backgroundColor: 'white'
+  },
+  listView: {
+    paddingTop: 20,
+  },
+  company: {
+    color: 'dimgrey',
+    fontSize: 15,
+    marginBottom: 8,
+    textDecorationLine: 'underline',
+  },
+  position: {
+    textAlign: 'center',
+    color: 'lightslategray',
+  },
+  line:{
+    borderBottomWidth: 1,
+    borderBottomColor: 'silver',
+    borderBottomLeftRadius: 5,
+    borderBottomRightRadius: 5,
+    marginBottom:4,
+  }
+});
+export default class JobDetail extends Component {
+  static get defaultProps() {
+    return {
+      title: 'Job Detail'
+    };
+  }
+  static propTypes = {
+    title: PropTypes.string.isRequired,
+    navigator: PropTypes.object.isRequired,
+  }
+
+constructor(props) {
+    super(props);
+    this.state = {
+      text: "",
+    };
+  }
+
+
+fetchData() {
+     fetch("https://hitch.herokuapp.com/api/addjob", {
+      method: 'POST',
+      headers: {
+            'Accept': 'application/json',
+            'Content-Type': 'application/json'
+        },
+      body: JSON.stringify({ 
+        user_email: 'tian@test.com', //REMEMBER TO CHANGE
+        company_name: this.props.m_n,
+        company_depart: "",  
+        position_title: "Software Development Test",
+        app_URL: this.props.m_w,
+      })
+    })
+      .then((response) => response.json())
+      .then((responseData) => {
+        if(responseData.result == "success"){
+          AlertIOS.alert(
+            "Add job succeeded"
+          );      
+        }else{
+          AlertIOS.alert(
+            "Add job failed"
+          )
+        }
+      })
+      .then(() => this.props.navigator.pop()) 
+      .done(); 
+  }
+
+  helpFind()
+  {
+    var data = this.props.employers;
+    var id = this.props.job_id;
+
+    for (var i = 0; i < data.length; i++)
+    {
+        if (data[i].id == id)
+        {
+          this.setState({
+            company: data[i].name,
+            overallRating : data[i].overallRating,
+            careerRating : data[i].careerOpportunitiesRating,
+            review : data[i].featuredReview.pros,
+            website : data[i].website,
+          });
+        }
+    }    
+  }
+
+  render() 
+  {
+    return (
+      <View style={{padding:20, marginTop: 50, flex: 1}}>
+        <View style={{flex:10,}}>
+
+        <View style = {styles.container}>
+        <Text style = {styles.company}>Company</Text>
+        <Text style = {styles.position}>{this.props.m_n}</Text>
+        </View>
+
+        <View style = {styles.container}>
+        <Text style = {styles.company}>Website</Text>
+        <Text style = {styles.position}>{this.props.m_w}</Text>
+        </View>
+
+        <View style = {styles.container}>
+        <Text style = {styles.company}>Overall Rating</Text>
+        <Text style = {styles.position}>{this.props.m_o}</Text>
+        </View>
+
+        <View style = {styles.container}>
+        <Text style = {styles.company}>Career Opportunities Rating</Text>
+        <Text style = {styles.position}>{this.props.m_c}</Text>
+        </View> 
+
+        <View style = {styles.container}>
+        <Text style = {styles.company}>Review</Text>
+        <Text style = {styles.position}>{this.props.m_r}</Text>
+        </View>
+
+        </View>
+
+        <View style={{flex:1}}>
+        <Button
+          backgroundColor = 'lightblue'
+          raised
+          large
+          icon={{name: 'cached'}}
+          title='Add This Job!' 
+          onPress = {() => this.fetchData()}/>
+        </View>
+      </View>
+
+    );
+  }
+}
+
